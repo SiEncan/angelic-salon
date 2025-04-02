37ccdaffea69b93f2504ecb19f14b5e354f4f732
SiEncan <adnanalvitomustaqim@gmail.com>
2025-04-02 12:56:49 +0700

    decorate login page

--- src/components/Dashboard/ManageServices.jsx
+++ src/components/Dashboard/ManageServices.jsx
@@ -158,7 +158,7 @@ const ManageServices = () => {
               <button
                 onClick={() => {
                   auth.signOut(); // Fungsi untuk logout
-                  navigate("/"); // Arahkan ke halaman login
+                  navigate("/login"); // Arahkan ke halaman login
                 }}
                 className="ml-4 bg-red-500 text-white py-2 px-4 rounded-full text-sm font-medium hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               >
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "../firebase";
+import { motion } from 'framer-motion';
+
+import character from '../assets/images/LoginCharacter.png';
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -65,25 +70,66 @@ function Login() {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-200 to-pink-300 p-4">
+      <div className="w-full max-w-5xl bg-white rounded-3xl shadow-lg grid grid-cols-1 md:grid-cols-2 overflow-hidden relative">
+        {/* Bagian kiri - Form Login */}
+        <div className="flex flex-col justify-center p-6 sm:p-10 relative">
+          <button 
+            onClick={() => navigate(-1)} 
+            className="flex items-center gap-2 text-lg text-black hover:text-gray-600 absolute top-4 left-4 sm:static sm:mb-4"
+            >
+            <ArrowLeftIcon className="w-5 h-5" />
+            <span>Back</span>
+          </button>
+          <div className="mt-10 sm:mt-0 flex flex-col items-center sm:items-start">
+            <h2 className="text-3xl font-bold">Log In</h2>
+            <p className="text-gray-500">Please visit the receptionist to get an account.</p>
+
+            {/* Bagian kanan - Ilustrasi */}
+            <div className="md:hidden flex py-4 w-full relative overflow-hidden justify-center items-center">
+              <div className="h-[25%] w-full absolute bottom-0 bg-[#C0DBEA] rounded-xl"></div>
+              <img src={character} alt="Character" className="relative relative z-10 w-3/5" />
+            </div>
+
+            <form onSubmit={handleLogin} className="mt-6 w-full">
+              <input
+                type="email"
+                placeholder="Email"
+                value={email} // Menghubungkan input dengan state
+                onChange={(e) => setEmail(e.target.value)} // Mengubah email saat input berubah
+                className="w-full p-3 border rounded-lg mb-4 bg-blue-100"
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password} // Menghubungkan input dengan state
+                onChange={(e) => setPassword(e.target.value)} // Mengubah password saat input berubah
+                className="w-full p-3 border rounded-lg mb-4 bg-blue-100"
+                required
+              />
+              <motion.button 
+                className="w-full text-white p-3 hover:scale-105 rounded-lg mt-2 transition-all duration-200"
+                style={{
+                  background: 'linear-gradient(to right, #ec4899, #f472b6)', // pink gradient awal
+                }}
+                whileHover={{
+                  background: 'linear-gradient(to right, #db2777,rgb(184, 64, 112))', // pink gradient lebih gelap
+                }}
+                transition={{ duration: 0.2 }} // Durasi transisi
+                >
+                LOGIN
+              </motion.button>
+            </form>
+          </div>
+        </div>
+
+        {/* Bagian kanan - Ilustrasi pada desktop */}
+        <div className="hidden md:flex items-center justify-center relative overflow-hidden">
+          <div className="absolute right-0 top-0 w-3/5 h-full bg-[#C0DBEA] rounded-l-3xl"></div>
+          <img src={character} alt="Character" className="w-3/4 left-5 py-5 relative z-10" />
+        </div>
+      </div>
     </div>
   );
 }
